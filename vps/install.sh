f97773cb800711f03ffb7f371885563006207557
Chris Stone <chris@sombrio.com>
2023-01-03 15:49:08 -0500

    Fix bugs in install.sh

--- vps/install.sh
+++ vps/install.sh
@@ -10,30 +10,36 @@ mkdir -p ./data/caddy/data
 mkdir -p ./data/caddy/config
 mkdir -p ./data/ghost/data
 [ ! -f "./data/caddy/Caddyfile" ] && cp caddy/Caddyfile ./data/caddy/Caddyfile
-[ ! -f ".env" ] && cp dotenv .env
+[ ! -f ".env" ] && cp dotenv.txt .env
 chmod 770 .env
 chown root:root .env
-docker network create --internal confined
-docker network create bbw
+docker network ls | grep confined
+if [[ $? == 1 ]]; then
+    docker network create --internal confined
+fi
+docker network ls | grep bbw
+if [[ $? == 1 ]]; then
+    docker network create bbw
+fi
 
 source .env
 
 echo "Generic configuration:"
-read -p "   Enter your domain name (${MYDOMAIN}) : " domain_name
+read -p "   Enter your domain name ($MYDOMAIN) : " domain_name
 if [ ! -z "$domain_name" ]; then
     MYDOMAIN=$domain_name
-    sed -i "s/MYDOMAIN=.*/MYDOMAIN=$domain_name/" .env
-    sed -i "0,/marker one/{s%https:.*%https://${MYDOMAIN}, https://www.${MYDOMAIN} \{%}" ./data/caddy/Caddyfile
+    sed -i "s/MYDOMAIN=.*/MYDOMAIN=\"$domain_name\"/" .env
+    sed -i "/marker zero/,/marker one/{s%https:.*%https://${MYDOMAIN}, https://www.${MYDOMAIN} \{%}" ./data/caddy/Caddyfile
+    sed -i "/marker one/,/marker two/{s%https://cloud.*%https://cloud.${MYDOMAIN}:443 \{%}" ./data/caddy/Caddyfile
+    sed -i "/marker two/,/marker three/{s%https://cloud.*%https://cloud.${MYDOMAIN}:8443 \{%}" ./data/caddy/Caddyfile
 fi
 
 echo "Caddy configuration:"
 read -p "   Enter VPN IP address of household nextcloud server (${MYNEXTCLOUDIP}) : " nextcloud_ip
 if [ ! -z "$nextcloud_ip" ]; then
     MYNEXTCLOUDIP=$nextcloud_ip
-    sed -i "s/MYNEXTCLOUDIP=.*/MYNEXTCLOUDIP=${nextcloud_ip}/" .env
-    sed -i "/marker one/,/marker two/{s%https://cloud.*%https://cloud.${MYDOMAIN}:443 \{%}" ./data/caddy/Caddyfile
+    sed -i "s/MYNEXTCLOUDIP=.*/MYNEXTCLOUDIP=\"${nextcloud_ip}\"/" .env
     sed -i "/marker one/,/marker two/{s/reverse_proxy.*/reverse_proxy ${MYNEXTCLOUDIP}:11000/}" ./data/caddy/Caddyfile
-    sed -i "/marker two/,/marker three/{s%https://cloud.*%https://cloud.${MYDOMAIN}:8443 \{%}" ./data/caddy/Caddyfile
     sed -i "/marker two/,/marker three/{s%reverse_proxy.*%reverse_proxy https://${MYNEXTCLOUDIP}:8080 \{%}" ./data/caddy/Caddyfile
 fi
 
@@ -41,7 +47,7 @@ echo "Ghost blog configuration:"
 read -p "   Enter SMTP host name ($MAILHOST) : " smtp_host
 if [ ! -z "$smtp_host" ]; then
     MAILHOST=$smtp_host
-    sed -i "s/MAILHOST=.*/MAILHOST=${smtp_host}/" .env
+    sed -i "s/MAILHOST=.*/MAILHOST=\"${smtp_host}\"/" .env
 fi
 read -p "   Enter SMTP port number ($MAILPORT) : " smtp_port
 if [ ! -z "$smtp_port" ]; then
@@ -51,17 +57,17 @@ fi
 read -p "   Enter email login user ($MAILUSER) : " email_user
 if [ ! -z "$email_user" ]; then
     MAILPORT=$email_user
-    sed -i "s/MAILUSER=.*/MAILUSER=${email_user}/" .env
+    sed -i "s/MAILUSER=.*/MAILUSER=\"${email_user}\"/" .env
 fi
-read -ps "   Enter email user password : " email_passw
+read -p "   Enter email user password : " -s email_passw
 if [ ! -z "$email_passw" ]; then
     MAILPASSW=$email_passw
-    sed -i "s/MAILPASSW=.*/MAILPASSW=${email_passw}/" .env
+    sed -i "s/MAILPASSW=.*/MAILPASSW=\"${email_passw}\"/" .env
 fi
-read -p "   Enter email from identity ($MAILFROM) : " email_identity
+read -p $'\n'"   Enter email from identity ($MAILFROM) : " email_identity
 if [ ! -z "$email_identity" ]; then
     MAILFROM=$email_identity
-    sed -i "s/MAILFROM=.*/MAILFROM=${email_identity}/" .env
+    sed -i "s/MAILFROM=.*/MAILFROM=\"${email_identity}\"/" .env
 fi
 
 cat data/caddy/Caddyfile
@@ -72,6 +78,8 @@ if [ ! -z "$answer" ]; then
     fi
 fi
 
+echo ""
+echo ""
 cat .env
 read -p "Please verify contents of docker compose environment variables file. Proceed (Y/n) ? " answer
 if [ ! -z "$answer" ]; then
